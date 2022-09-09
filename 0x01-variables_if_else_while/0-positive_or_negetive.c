1e46da3ddaed76df09f2d4fb62b8cffddf21a58c
muhanelwa01 <muhanelwa01@gmail.com>
2022-09-09 10:07:34 -0700

    Positive or negetive

---
+++ 0x01-variables_if_else_while/0-positive_or_negetive.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ n is a random value 
+**/
+
+/**
+ *n has a random value each time when executed
+ *n can either be positive or negetive
+ *if else n is zero
+ **/
+
+int main(void) 
+{
+  int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  /**
+    printf("%n=1"positive;"%n=0"zero;"%n=-1"negetive; n++;
+ **/
+  return (0); 
+}
