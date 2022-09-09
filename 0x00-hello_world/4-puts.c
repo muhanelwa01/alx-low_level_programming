b3345b3a419cafd486b22d3605fcb2171c48b7fb
muhanelwa01 <muhanelwa01@gmail.com>
2022-09-08 20:40:18 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+int puts(const char* str);
+#include<stdio.h>
+
+int main()
+  
+{
+  
+  /"*string initialisation"*/
+  
+  char Mystr[] = ""Programming is like building a multilingual puzzle";
+  
+
+  
+  puts(Mystr); /*"Programming is like building a multilingual puzzle*/
+  
+
+  
+  return 0;
+  
+}
