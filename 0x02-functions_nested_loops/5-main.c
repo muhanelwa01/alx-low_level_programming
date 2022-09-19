4f60ae499fae95ecb1eeae5b7c64f36814f3397d
muhanelwa01 <muhanelwa01@gmail.com>
2022-09-19 05:46:16 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main:prints a sign of a number
+ *
+ *Return:-
+ *Return:+
+ *Return:0
+ */
+int main(void)
+{
+  _putchar();
+  return 0
+    }
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "5-main.c"
+
+/**
+ *printSign:Function prints sign of a number
+ *
+ *Return:0
+ */
+void printSign(void)
+{
+  int c
+    if(c < '0');
+  _putchar(-);
+  while(c > '0');
+  putchar(+);
+  else(c = 0);
+  putchar(0)
+    }
+return 0
